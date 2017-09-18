40441ae02d5e773fc679e61b8dc57a0accedd9aa
Admin <Admin@DESKTOP-K32JQUJ>
2017-09-18 23:16:33 +0530

    Bit Wise Assignment.

---
+++ connectorModule/BitWise.java
@@ -0,0 +1,15 @@
+
+public class BitWise {
+public static void main(String args[])
+{
+	int fno = 11;
+	int sno = 12;
+	int tno = 13;
+	System.out.println("fno & sno :"+ (fno & sno));
+	
+	boolean result = sno > fno && tno >sno;
+	System.out.println("&& operator output:"+ result);
+	
+	
+}
+}
